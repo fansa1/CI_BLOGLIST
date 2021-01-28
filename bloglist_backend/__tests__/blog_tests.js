6e90823ebaf33d82b23651ac79e347338f6bf8b8
Samuel <samuel.fanta@helsinki.fi>
2021-01-28 17:38:47 +0200

    package updated

--- bloglist_backend/__tests__/blog_tests.js
+++ bloglist_backend/__tests__/blog_tests.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const Blog = require('../models/Blog')
+const Blog = require('../models/blog')
 const supertest = require('supertest')
 const app = require('../app')
 
