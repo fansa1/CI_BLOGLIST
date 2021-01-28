a950713cc1ee12f74e6892d2ec37be38e100f6a2
Samuel <samuel.fanta@helsinki.fi>
2021-01-28 18:05:02 +0200

    updated blog

--- bloglist_backend/__tests__/blog_tests.js
+++ bloglist_backend/__tests__/blog_tests.js
@@ -1,9 +1,30 @@
 const mongoose = require('mongoose')
-const Blog = require('../files_for_tests/Blog')
 const supertest = require('supertest')
-const app = require('../files_for_tests/app')
+const app = require('../app')
 
 const api = supertest(app)
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
 
 test('All blogs are returned', async () => {
  const response =   await api.get('/api/blogs')
