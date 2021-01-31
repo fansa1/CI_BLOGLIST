f09b4b6429f968c0eabcc7840d029f220ea91b85
Samuel <samuel.fanta@helsinki.fi>
2021-01-31 13:37:07 +0200

    new app

---
+++ bloglist_backend/build/precache-manifest.658e8e9371704b7134616610740dbfe0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f62ce13ba8330f5f127f79e3fd27353",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5b731a32b594606b6d7",
+    "url": "/static/js/2.2be706e4.chunk.js"
+  },
+  {
+    "revision": "e739fc37833f158457c2",
+    "url": "/static/js/main.2da6b6d8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
