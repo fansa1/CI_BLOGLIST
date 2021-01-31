9f4898770b673225ca4e9438ff807224259cadf3
Samuel <samuel.fanta@helsinki.fi>
2021-01-31 10:37:27 +0200

    changed index file

--- bloglist_backend/index.js
+++ bloglist_backend/index.js
@@ -1,9 +1,11 @@
 const app = require('./app') 
 const http = require('http')
-const config = require('./utils/config')
+require('dotenv').config()
 
 const server = http.createServer(app)
 
-server.listen(config.PORT, () => {
-  console.log(`Server running on port ${config.PORT}`)
+let PORT = process.env.PORT || 3003
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
 })
